b42f75c355caa0458fab46088bc71716fe01e868
peculidar <darirawr92@gmail.com>
2020-06-30 15:14:28 +0300

    работа над ошибками

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,5 +1,7 @@
 const contactUsLink = document.querySelector(".modal-button");
+const contactUsPopup = document.querySelector(".contact-form-article");
 
 contactUsLink.addEventListener("click", function(evt) {
     evt.preventDefault();
+
 });
